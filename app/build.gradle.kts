956bcf0e018b7e30ad26c6216a8a6ce0b2972b2b
Martin <martin@dasos.com>
2026-08-28 19:13:12 +0100

    chore(release): bump version to 0.6.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.chirp"
         minSdk = 26
         targetSdk = 35
-        versionCode = 6
-        versionName = "0.5.3"
+        versionCode = 7
+        versionName = "0.6.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
